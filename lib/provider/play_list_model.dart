8ccd967510c5bd551617abe8d14fcb62c8903217
dance41ife <1656832171@qq.com>
2020-10-16 11:04:34 +0800

    测试新分支上传

---
+++ lib/provider/play_list_model.dart
@@ -0,0 +1,30 @@
+import 'package:flutter_shop/entity/music_list_structure.dart';
+import 'package:flutter_shop/utils/share_preferences_util.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class PlayListService{
+  List<MusicListItem> _playList;
+  int currIndex;
+
+  void addToPlayList(MusicListItem item){
+    _playList.add(item);
+  }
+
+  MusicListItem replacePlayList(List<MusicListItem> list,int index){
+    _playList = list;
+    currIndex = index;
+    return _playList.elementAt(index);
+  }
+
+  MusicListItem getNextSong(){
+    currIndex = currIndex>=_playList.length ? 0 :currIndex+1;
+    return _playList.elementAt(currIndex);
+  }
+
+  MusicListItem getPreviousSong(){
+    currIndex = currIndex <=0 ? _playList.length - 1 :currIndex-1;
+    return _playList.elementAt(currIndex);
+  }
+
+
+}
